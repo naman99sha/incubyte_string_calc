7254f448dab31e0659a8ded541a891f1a802a19c
Naman Sharma <naman99sha@gmail.com>
2025-06-13 09:35:38 +0530

    Created initial logic to add numbers

--- string_calc.py
+++ string_calc.py
@@ -3,3 +3,9 @@ import re
 def add(number: str) -> int:
     if(not number):
         return 0
+    delimeter = ','
+
+    numbers = re.split(delimeter, number)
+    num_list = list(map(int, filter(None, numbers)))
+
+    return sum(num_list)
