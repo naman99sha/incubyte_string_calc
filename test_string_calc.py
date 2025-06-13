a94e309eda148270d870ae25e49eb5bcb2dd8389
Naman Sharma <naman99sha@gmail.com>
2025-06-13 09:37:24 +0530

    Added test for negative number exception

--- test_string_calc.py
+++ test_string_calc.py
@@ -13,3 +13,11 @@ def test_two_numbers():
 
 def test_multiple_numbers():
     assert add("1,2,3,4,5") == 15
+
+def test_negative_number_raises_exception():
+    with pytest.raises(ValueError, match="negative numbers not allowed -1"):
+        add("1,-1")
+
+def test_multiple_negative_numbers_exception():
+    with pytest.raises(ValueError, match="negative numbers not allowed -1,-2"):
+        add("1,-1,-2")
